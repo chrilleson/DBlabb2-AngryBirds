f66f6da15b93fa861b1d62f8b6e5d3ceba4d342d
Knut <knut@lyven.se>
2018-01-15 15:34:53 +0100

    Updated Score.cs

--- Labb2/AngryBirds_Labb2/AngryBirds_Labb2/Score.cs
+++ Labb2/AngryBirds_Labb2/AngryBirds_Labb2/Score.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace AngryBirds_Labb2
 {
     class Score
     {
+        [Key]
+        public int ScoreID { get; set; }
+        public virtual Level Level { get; set; }
+        public virtual Player Player { get; set; }
+        public int Highscore { get; set; }
     }
 }

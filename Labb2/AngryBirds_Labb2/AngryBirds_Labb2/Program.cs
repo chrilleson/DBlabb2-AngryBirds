864432716d5c1095fe4bb52fb93066589a59466c
Knut <knut@lyven.se>
2018-01-18 16:27:10 +0100

    Made a function to get the users ID. Started on how to update score.

--- Labb2/AngryBirds_Labb2/AngryBirds_Labb2/Program.cs
+++ Labb2/AngryBirds_Labb2/AngryBirds_Labb2/Program.cs
@@ -10,6 +10,31 @@ namespace AngryBirds_Labb2
     {
         static void Main(string[] args)
         {
+
+
         }
+
+        private static void UpdateScore(ScoreContext context)
+        {
+            Console.Clear();
+            Console.WriteLine("Type the Username you want to update: ");
+            string usernameInput = Console.ReadLine();
+            bool searchresult = context.Players.Any(x => x.PlayerName == usernameInput);
+
+            if(searchresult)
+            {
+                int usernameID = usernameID
+            }
+        }
+
+        //Look if the users ID exists.
+        private static int GetUserID(ScoreContext context, string username)
+        {
+            var getID = context.Players.Where(u => u.PlayerName == username).FirstOrDefault();
+            return getID.PlayerID;
+        }
+
+
+
     }
 }

2cf50ac5c527a0a643979d96254c9d0c0f941beb
Knut <knut@lyven.se>
2018-01-18 16:05:11 +0100

    Finished ScoreContext

--- Labb2/AngryBirds_Labb2/AngryBirds_Labb2/ScoreContext.cs
+++ Labb2/AngryBirds_Labb2/AngryBirds_Labb2/ScoreContext.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace AngryBirds_Labb2
 {
-    class ScoreContext
+    public class ScoreContext : DbContext
     {
+        private const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog = AngryBirdsDB; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public ScoreContext() : base(connectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Player>()
+                .HasMany(s => s.Scoreboard)
+                .WithRequired(p => p.Player);
+            modelBuilder.Entity<Level>()
+                .HasMany(s => s.Scoreboard)
+                .WithRequired(l => l.Level);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Level> Levels { get; set; }
+        public DbSet<Score> Scores { get; set; }
+
     }
 }
